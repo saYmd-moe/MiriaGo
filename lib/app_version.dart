e552a9dca56e454f54d256cc747c64d405d91c39
BilyHurington <bilyhurington@gmail.com>
2026-08-09 02:02:58 +0900

    chore: bump version to 1.1.5+22

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,6 +1,6 @@
 import 'package:package_info_plus/package_info_plus.dart';
 
-const miriagoAppVersion = '1.1.4+21';
+const miriagoAppVersion = '1.1.5+22';
 
 Future<String> loadAppVersionLabel() async {
   final info = await PackageInfo.fromPlatform();
