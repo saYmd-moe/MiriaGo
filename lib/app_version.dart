66ddfb5d37957180b767927839a318082a418bc0
saymd-moe <liuymyz@foxmail.com>
2026-08-27 14:43:36 +0800

    release: prepare v1.1.7

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,6 +1,6 @@
 import 'package:package_info_plus/package_info_plus.dart';
 
-const miriagoAppVersion = '1.1.6+23';
+const miriagoAppVersion = '1.1.7+24';
 
 Future<String> loadAppVersionLabel() async {
   final info = await PackageInfo.fromPlatform();
